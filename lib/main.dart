c9b0f8faaf04cdadde4f392dba5b94a9ff924b75
JHON WILLIAM TEJADA DURANGO <jhontejada112531@correo.itm.edu.co>
2021-10-23 15:01:09 -0500

    HomeScreen Configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:partial1_elephantapp/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.purple[300],
-          leading: Image.network("https://www.besoftsocks.com/wp-content/uploads/2018/07/Elefante.png",
-              color: Colors.white,
-          ),
-          title: Container(
-            alignment: Alignment.center,
-            child: Text("ElephantApp")
-          ),          
-          elevation: 20,
-          shadowColor: Colors.purple[100],        
-        ),
-        body: Center(
-          child: Container(
-            child: TextButton(
-              child: Text("Ver elefantes"),
-              onPressed: () {},
-            ),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Elephant App',
+      home: HomeScreen()
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _showLoader = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple[300],
+          leading: Image.network("https://www.besoftsocks.com/wp-content/uploads/2018/07/Elefante.png",
+              color: Colors.white,
+          ),
+          title: Container(
+            alignment: Alignment.center,
+            child: Text("ElephantApp")
+          ),          
+          elevation: 20,
+          shadowColor: Colors.purple[100],        
+        ),
+        body: _getBody(),
+      );
+  }
+
+  Widget _getBody() {
+    return Stack(
+      children: <Widget> [
+        SingleChildScrollView(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              SizedBox(height: 50,),
+              _showImage(),
+              SizedBox(height: 20,),
+              Center(
+                child: Container(
+                  child: 
+                    _showButton(), 
+                ),
+              ),
+            ],        
+          )
+        ),
+      ],
+      
+    );
+  }
+
+  Widget _showButton() {
+    return Expanded(
+      child: ElevatedButton(
+        child: const Text('Ver Elefantes'),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith<Color>(
+            (Set<MaterialState> states) {
+              return Color(0xFFE1BEE7);
+            }
+          ),
+        ),
+        onPressed: () => _mostrarLista()
+      ),
+    );
+  }
+
+  Widget _mostrarLista() {
+    return Container(
+      
+    );
+  }
+
+  Widget _showImage() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(175),
+      child: const FadeInImage(
+        placeholder: AssetImage('assets/elefante.jpg'),
+        image: NetworkImage("https://lh3.googleusercontent.com/proxy/UUFkIe3eGL64fBSBjRNlcmszXaaFMWWhxrhGI4DygugxobDZXS76pjhNQdLsD6MDQ6VhE0U8LRBtPSdsESXL5Luo6V0ZoGpeu2WPKuYph5BYZUQ2uZl4LK1SR3E-"),
+        height: 350,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+}
